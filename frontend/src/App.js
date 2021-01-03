587f1197b9251142935cfde382b45fb876c2c4d3
SubratShrestha <subrat.shrestha3@gmail.com>
2021-01-03 12:44:26 +1100

    added form, needs to connect to backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,113 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import { Form, FormGroup, FormControl, ControlLabel, DatePicker, RadioGroup, Radio, InputNumber, Schema, Button } from 'rsuite';
+import 'rsuite/dist/styles/rsuite-default.css';
 
 function App() {
+  const { StringType, NumberType, DateType } = Schema.Types;
+  const [formValue, setFormValue] = useState({});
+  const [form, setFormRef] = useState(null);
+
+  const model = Schema.Model({
+    firstname: StringType().isRequired('First name required to make investment'),
+    lastname: StringType().isRequired('Last name required to make investment'),
+    dob: DateType().isRequired('valid date required'),
+    email: StringType()
+      .isEmail('please enter a valid email')
+      .isRequired('Email required to make investment'),
+    number: NumberType().isRequired('Number required to make investment'),
+    type: StringType().isRequired('Type of investment required'),
+    amount: NumberType().isRequired('Amount required to make investment'),
+  });
+
+  const fixDate = (date) => {
+    if (!date) {
+      return;
+    }
+    return `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}`;
+  }
+
+  useEffect(() => {
+    console.log(formValue);
+  }, [formValue]);
+
+  const handleSubmit = () => {
+    if (!form.check()) {
+      return;
+    }
+    console.log('submitting')
+    //   fetch('http://localhost:8000/api/invest/new', {
+    //     method: 'POST',
+    //     headers: {
+    //       'Content-Type': 'application/json',
+    //     },
+    //     body: JSON.stringify({
+    //       ...formValue
+    //     }).then(res => res.json()).then(data => console.log(data));
+    //   })
+    // }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+        <Form
+          onChange={formValue => setFormValue({ ...formValue, dob: fixDate(formValue.dob) })}
+          model={model}
+          ref={(ref) => setFormRef(ref)}
         >
-          Learn React
-        </a>
-      </header>
+          <FormGroup>
+            <ControlLabel>First Name</ControlLabel>
+            <FormControl name="firstname" />
+          </FormGroup>
+          <FormGroup>
+            <ControlLabel>Last name</ControlLabel>
+            <FormControl name="lastname" />
+          </FormGroup>
+          <CustomField
+            accepter={DatePicker}
+            name="dob"
+            label="Date of birth"
+          />
+          <FormGroup>
+            <ControlLabel>
+              Email
+            </ControlLabel>
+            <FormControl name="email" type="email" />
+          </FormGroup>
+          <FormGroup>
+            <ControlLabel>
+              Phone
+            </ControlLabel>
+            <FormControl name="number" type="number" />
+          </FormGroup>
+          <CustomField
+            name="type"
+            label="type"
+            accepter={RadioGroup}
+          >
+            <Radio value={"premium"}>Premium</Radio>
+            <Radio value={"select"}>Select</Radio>
+          </CustomField>
+          <CustomField
+            accepter={InputNumber}
+            name="amount"
+            prefix={"$"}
+          />
+          <FormGroup>
+            <Button appearance="primary" onClick={handleSubmit}>Submit</Button>
+          </FormGroup>
+        </Form>
+      </div>
     </div>
+  );
+}
+
+function CustomField({ accepter, name, label, ...props }) {
+  return (
+    <FormGroup>
+      <ControlLabel>{label}</ControlLabel>
+      <FormControl name={name} accepter={accepter} {...props} />
+    </FormGroup>
   );
 }
 
